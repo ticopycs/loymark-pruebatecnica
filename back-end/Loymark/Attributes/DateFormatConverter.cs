dfe7bb7c5fb8d7fa8db6c662db7c60afc733b20b
Martin Reale <ticopycs@gmail.com>
2023-04-08 10:56:04 -0300

    initial backend commit

---
+++ back-end/Loymark/Attributes/DateFormatConverter.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace Loymark.Attributes
+{
+    public class DateFormatConverter : IsoDateTimeConverter
+    {
+        public DateFormatConverter(string format)
+        {
+            DateTimeFormat = format;
+        }
+    }
+}
---
+++ back-end/Loymark/Models/Base.cs
@@ -0,0 +1,18 @@
+﻿using Loymark.Attributes;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loymark.Models
+{
+    public class Base
+    {
+        [StringLength(64)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [JsonConverter(typeof(DateFormatConverter), "yyyy-MM-ddTHH:mm:ss.fffZ")]
+        public DateTime? InsertedAt { get; set; }
+
+        [JsonConverter(typeof(DateFormatConverter), "yyyy-MM-ddTHH:mm:ss.fffZ")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
